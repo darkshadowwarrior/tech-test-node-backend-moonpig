bb6f255c447714e83500f699878edfe94513a172
Ian Richards <cdeveloper@hotmail.co.uk>
2025-05-09 16:35:12 +0100

    updated integration tests and handlers
    updated integration tests and handlers

--- src/handlers/__tests__/server.test.ts
+++ src/handlers/__tests__/server.test.ts
@@ -20,24 +20,98 @@ describe('server', () => {
 
   //TODO:  Make sure you write some tests to cover errors like 500 and 400 errors
 
-  test('returns matching card based opn given cardId and sizeId', async () => {
+  test('returns matching card based on given cardId and sizeId', async () => {
+    const expected = {
+      title: 'card 1 title',
+      size: "gt",
+      availableSizes: [{
+        "id": "sm",
+        "title": "Small"
+      },
+      {
+        "id": "md",
+        "title": "Medium"
+      },
+      {
+        "id": "gt",
+        "title": "Giant"
+      }],
+      imageUrl: '/front-cover-portrait-1.jpg',
+      price: "£4.00",
+      pages: [
+        {
+          height: 600,
+          id: "template001",
+          imageUrl: "/front-cover-portrait-1.jpg",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template002",
+          imageUrl: "",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template003",
+          imageUrl: "",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template004",
+          imageUrl: "/back-cover-portrait.jpg",
+          width: 300,
+        }
+      ]
+    };
+
     const response = await request(app).get('/cards/card001/gt')
 
     expect(response.status).toBe(200)
-    expect(response.body).toEqual(expect.objectContaining({
-      title: 'card 1 title',
-    }))
+    expect(response.body).toEqual(expect.objectContaining(expected))
   })
 
   //test included in the repo but not mentioned in the README
   // unsure if this needs addressing but will do it just in case
-  test('returns matching card title', async () => {
+  test('returns matching card based on given cardId', async () => {
+    const expected = {
+      title: 'card 1 title',
+      price: 200,
+      availableSizes: ["sm", "md", "gt"],
+      imageUrl: '/front-cover-portrait-1.jpg',
+      pages: [
+        {
+          height: 600,
+          id: "template001",
+          imageUrl: "/front-cover-portrait-1.jpg",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template002",
+          imageUrl: "",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template003",
+          imageUrl: "",
+          width: 300,
+        },
+        {
+          height: 600,
+          id: "template004",
+          imageUrl: "/back-cover-portrait.jpg",
+          width: 300,
+        }
+      ]
+    };
+    
     const response = await request(app).get('/cards/card001')
 
     expect(response.status).toBe(200)
-    expect(response.body).toEqual(expect.objectContaining({
-      title: 'card 1 title',
-    }))
+    expect(response.body).toEqual(expect.objectContaining(expected))
   })
 })
 
--- src/handlers/server.ts
+++ src/handlers/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getCards } from "../usecases/get-cards/get-cards";
+import { getCardById } from "../usecases/get-card-by-id/get-card-by-id";
 
 export const app = express()
 
@@ -11,10 +12,18 @@ app.get('/cards', async (req, res) => {
   res.status(200).send(cards);
 })
 
-app.get('/cards/:cardId', () => {
+app.get('/cards/:cardId', async (req, res) => {
   // respond with card by id
+  const params = req.params;
+
+  const cards = await getCardById(params.cardId);
+  res.status(200).send(cards);
 })
 
-app.get('/cards/:cardId/:sizeId?', () => {
+app.get('/cards/:cardId/:sizeId?', async (req, res) => {
   // respond with card by id
+  const params = req.params;
+
+  const cards = await getCardById(params.cardId, params.sizeId);
+  res.status(200).send(cards);
 })
