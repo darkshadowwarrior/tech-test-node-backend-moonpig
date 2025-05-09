11812cdf54f4ad2a997cdd0f0e201c570d95f5cb
Ian Richards <cdeveloper@hotmail.co.uk>
2025-05-09 15:09:44 +0100

    implemented getCards
    added implementation details to get cards
    added error handling and tests

--- src/usecases/get-cards/__tests__/get-cards.test.ts
+++ src/usecases/get-cards/__tests__/get-cards.test.ts
@@ -3,7 +3,7 @@ import { Card } from "../../../types/card";
 import { cardsService } from "../../../services/card-service";
 import { templatesService } from "../../../services/template-service";
 import { mocked } from 'jest-mock'
-import { mockDeep, objectContainsValue } from 'jest-mock-extended';
+import { mockDeep } from 'jest-mock-extended';
 
 jest.mock('../../../services/card-service')
 jest.mock('../../../services/template-service')
@@ -47,6 +47,45 @@ describe('getCards', () => {
       
       const received = await getCards();
 
-      expect(received[0]).toEqual({title: 'card title 1'});
+      expect(received[0]).toEqual({
+        imageUrl: "/front-cover-portrait-1.jpg",
+        title: "card 1 title",
+        url: "card/card001",
+      });
+    })
+
+    test('should catch and rethrow error if card service errors', async () => {
+      mockCardService.mockRejectedValue(new Error("card service returned 500"));
+
+      expect(getCards).rejects.toEqual(new Error("Unable to fetch cards"));
+    })
+
+    test('should catch and rethrow error if template service errors', async () => {
+      mockCardService.mockResolvedValue(mockDeep<Array<Card>>([{
+        "id": "card001",
+        "title": "card 1 title",
+        "pages": [
+          {
+            "title": "Front Cover",
+            "templateId": "template001"
+          },
+          {
+            "title": "Inside Left",
+            "templateId": "template002"
+          },
+          {
+            "title": "Inside Right",
+            "templateId": "template003"
+          },
+          {
+            "title": "Back Cover",
+            "templateId": "template004"
+          }
+        ]
+      }]))
+
+      mockTemplatesService.mockRejectedValue(new Error("template service returned 500"));
+      
+      expect(getCards).rejects.toEqual(new Error("Unable to fetch cards"));
     })
 })
--- src/usecases/get-cards/get-cards.ts
+++ src/usecases/get-cards/get-cards.ts
@@ -1,5 +1,20 @@
-import { Card } from "../../types/card"
+import { cardsService } from "../../services/card-service";
+import { templatesService } from "../../services/template-service";
 
 export const getCards = async () => {
-    return [{title: 'card title 1'} as Card]
+    try {
+        const [cards, templates] = await Promise.all([cardsService(), templatesService()]);
+
+        return cards.map(card => {
+            const template = templates.find(template => template.id == card.pages[0].templateId);
+
+            return {
+                title: card.title,
+                imageUrl: template.imageUrl,
+                url: `card/${card.id}`
+            }
+        })
+    } catch (err) {
+        throw new Error("Unable to fetch cards");
+    }
 }
